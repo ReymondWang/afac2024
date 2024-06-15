75d647f5fb97ae676a95dde86d60437d9725a75d
ReymondWang <zisetianguang@sina.com>
2024-06-15 21:19:06 +0800

    提交微调后的模型的评估和baseline评估

--- ChatGLM3-6B-QLoRA/readme.md
+++ ChatGLM3-6B-QLoRA/readme.md
@@ -3,7 +3,7 @@
 进入本项目目录，训练启动命令如下：
 
 ```shell
-python3 train_qlora.py \
+python train_qlora.py \
 --train_args_json QLoRA.json \
 --model_name_or_path ../../Model/THUDM/chatglm3-6b \
 --train_data_path data/train.json \
@@ -11,4 +11,9 @@ python3 train_qlora.py \
 --lora_rank 4 \
 --lora_dropout 0.05 \
 --compute_dtype fp32
+```
+
+## 查看训练loss
+```shell
+tensorboard --logdir runs --port 'your port' --bind_all
 ```
---
+++ ChatGLM3-6B-QLoRA/tools/standard_name_utils.py
@@ -0,0 +1,118 @@
+import json
+from difflib import get_close_matches
+
+SO_STR = "query中提到的产品标准名可能是："
+SPLITOR = "、"
+
+RES_API_KEY = "relevant APIs"
+RES_KEY = "result"
+RES_TOOL_NAME_KEY = "tool_name"
+RES_API_NAME_KEY = "api_name"
+RES_PARAM_KEY = "required_parameters"
+
+
+def get_input_standard_names(input: str) -> list:
+    r"""
+        从输入的Prompt中获得基金标准名
+
+        Args:
+            input (`str`):
+                输入的Prompt字符串
+        """
+    SOS = input.index(SO_STR) + len(SO_STR)
+    standard_name_str = input[SOS:]
+    standard_name_arr = standard_name_str.split(SPLITOR)
+    return standard_name_arr
+
+
+def optimize_parameters(output: str, fund_standard_name: list, stock_standard_name: list) -> str:
+    r"""
+        优化LLM模型原始的输出
+
+        Args:
+            output (`str`):
+                LLM原始输出的结果
+            input (`str`):
+                输入的Prompt字符串
+                
+        Result:
+            优化后的LLM的输出
+        """
+    
+    raw = json.loads(output)
+
+    api_list = raw[RES_API_KEY]
+    for api in api_list:
+        # api[RES_PARAM_KEY] = reorder(api[RES_PARAM_KEY])
+        if api[RES_TOOL_NAME_KEY] == "基金查询" and api[RES_API_NAME_KEY] == "查询代码":
+            api[RES_PARAM_KEY] = switch_standard_name(api[RES_PARAM_KEY], fund_standard_name)
+        elif api[RES_TOOL_NAME_KEY] == "股票查询" and api[RES_API_NAME_KEY] == "查询代码":
+            api[RES_PARAM_KEY] = switch_standard_name(api[RES_PARAM_KEY], stock_standard_name)
+            
+    raw[RES_API_KEY] = api_list
+    return json.dumps(raw, ensure_ascii=False)
+    
+    
+def reorder(params: list) -> list:
+    r"""
+        对调用函数的参数重新排序
+        将依赖api的结果按照顺序进行排序，并且放到最前面，其他的参数顺序不变。
+        
+        Args:
+            params (`list`):
+                原始函数的参数
+        
+        Result:
+            重新排序以后的参数
+    """
+    
+    if params.count == 1:
+        return params
+    
+    rely_api_res = []
+    oth = []
+    for param in params:
+        if isinstance(param, str) and param.startswith("api_"):
+            rely_api_res.append(param)
+        else:
+            oth.append(param)
+            
+    rely_api_res.sort()
+
+    return rely_api_res + oth
+   
+            
+def switch_standard_name(raw_list: list, standard_name_list: list) -> list:
+    r"""
+        将参数中的参数修改为标准名称
+        
+        Args:
+            params (`raw_list`):
+                原始函数的参数列表
+            params (`standard_name_list`):
+                标准名称列表
+        
+        Result:
+            替换以后的参数
+    """
+    
+    res = []
+    for raw_item in raw_list:
+        if isinstance(raw_item, list):
+            res.append(switch_standard_name(raw_item, standard_name_list))
+        else:
+            switch_name_list = get_close_matches(raw_item, standard_name_list, n=1)
+            if len(switch_name_list) > 0:
+                res.append(switch_name_list[0])
+            else:
+                res.append(raw_item)
+    return res
+
+  
+if __name__ == "__main__":
+    test_input = "我想知道国金核心资产A的基金经理是谁，以及他的年化回报率和管理的总规模 \n query中提到的产品标准名可能是：国金核心资产一年持有期混合型证券投资基金C类、国金核心资产一年持有期混合型证券投资基金A类、富国大盘核心资产混合型证券投资基金、国联安核心资产策略混合型证券投资基金、创金合信核心资产混合型证券投资基金C类、创金合信核心资产混合型证券投资基金A类、金鹰核心资源混合型证券投资基金C类、金鹰核心资源混合型证券投资基金A类、金元顺安核心动力混合型证券投资基金、汇安核心资产混合型证券投资基金C类、汇安核心资产混合型证券投资基金A类、富国核心趋势混合型证券投资基金C类、富国核心趋势混合型证券投资基金A类、国投瑞银核心企业混合型证券投资基金、华夏核心资产混合型证券投资基金C类、华夏核心资产混合型证券投资基金A类、国联安核心优势混合型证券投资基金A类、民生加银核心资产股票型证券投资基金C类、民生加银核心资产股票型证券投资基金A类、国联核心成长灵活配置混合型证券投资基金、博时核心资产精选混合型证券投资基金C类、博时核心资产精选混合型证券投资基金A类、创金合信量化核心混合型证券投资基金C类、创金合信量化核心混合型证券投资基金A类、创金合信核心价值混合型证券投资基金C类、创金合信核心价值混合型证券投资基金A类、鑫元核心资产股票型发起式证券投资基金C类、鑫元核心资产股票型发起式证券投资基金A类、海富通消费核心资产混合型证券投资基金C类、海富通消费核心资产混合型证券投资基金A类、富国核心优势混合型发起式证券投资基金C类、富国核心优势混合型发起式证券投资基金A类、光大保德信核心资产混合型证券投资基金C类、光大保德信核心资产混合型证券投资基金A类、交银施罗德核心资产混合型证券投资基金C类、交银施罗德核心资产混合型证券投资基金A类、招商资管核心优势混合型集合资产管理计划C类、招商资管核心优势混合型集合资产管理计划A类、国寿安保核心产业灵活配置混合型证券投资基金、金信核心竞争力灵活配置混合型证券投资基金A类、汇添富大盘核心资产增长混合型证券投资基金C类、汇添富大盘核心资产增长混合型证券投资基金A类、国泰核心价值两年持有期股票型证券投资基金C类、国泰核心价值两年持有期股票型证券投资基金A类、国泰金鹿混合型证券投资基金、国联安核心趋势一年持有期混合型证券投资基金C类、国联安核心趋势一年持有期混合型证券投资基金A类、中金安心回报灵活配置混合型集合资产管理计划C类、中金安心回报灵活配置混合型集合资产管理计划A类、银华全球核心优选证券投资基金"
+    # get_input_standard_names(test_input)
+    
+    test_output = "{\"relevant APIs\": [{\"api_id\": \"0\", \"api_name\": \"查询代码\", \"required_parameters\": [[\"国金核心资一年持有期混合型证券投资基金C类\"]], \"rely_apis\": [], \"tool_name\": \"基金查询\"}, {\"api_id\": \"1\", \"api_name\": \"查询基金经理\", \"required_parameters\": [\"api_1的结果\", \"api_0的结果\"], \"rely_apis\": [\"0\"], \"tool_name\": \"基金查询\"}, {\"api_id\": \"2\", \"api_name\": \"查询基金经理年化回报率\", \"required_parameters\": [\"api_1的结果\"], \"rely_apis\": [\"1\"], \"tool_name\": \"基金查询\"}, {\"api_id\": \"3\", \"api_name\": \"查询基金经理管理规模\", \"required_parameters\": [\"api_1的结果\"], \"rely_apis\": [\"1\"], \"tool_name\": \"基金查询\"}], \"result\": [\"api_1的结果\", \"api_2的结果\", \"api_3的结果\"]}"
+    res = optimize_parameters(test_output, test_input)
+    print(res)
