b9f4ff312b8f42d3bb1f7c8d632a8a8ac98dd1e8
ReymondWang <zisetianguang@sina.com>
2024-07-12 23:37:47 +0800

    上传dpo代码

--- ChatGLM3-6B-Finetuning/tools/common_utils.py
+++ ChatGLM3-6B-Finetuning/tools/common_utils.py
@@ -1,5 +1,15 @@
+import os
+import getpass
 import json
+import random
+import torch
+import socket
+import numpy as np
+import torch.distributed as dist
 from colorama import Fore, Style
+from typing import Dict, Union, Type, List
+from datetime import datetime
+
 
 def highlight_diff(str1, str2):
     r"""
@@ -40,6 +50,116 @@ def read_jsonl(file_path) -> list:
         for line in f:
             data.append(json.loads(line.strip()))
     return data
+
+
+def get_local_dir(prefixes_to_resolve: List[str]) -> str:
+    """Return the path to the cache directory for this user."""
+    for prefix in prefixes_to_resolve:
+        if os.path.exists(prefix):
+            return f"{prefix}/{getpass.getuser()}"
+    os.makedirs(prefix)
+    return f"{prefix}/{getpass.getuser()}"
+
+
+def pad_to_length(tensor: torch.Tensor, length: int, pad_value: Union[int, float], dim: int = -1) -> torch.Tensor:
+    if tensor.size(dim) >= length:
+        return tensor
+    else:
+        pad_size = list(tensor.shape)
+        pad_size[dim] = length - tensor.size(dim)
+        return torch.cat([tensor, pad_value * torch.ones(*pad_size, dtype=tensor.dtype, device=tensor.device)], dim=dim)
+
+
+def rank0_print(*args, **kwargs):
+    """Print, but only on rank 0."""
+    if not dist.is_initialized() or dist.get_rank() == 0:
+        print(*args, **kwargs)
+
+
+def all_gather_if_needed(values: torch.Tensor, rank: int, world_size: int) -> torch.Tensor:
+    """Gather and stack/cat values from all processes, if there are multiple processes."""
+    if world_size == 1:
+        return values
+
+    all_values = [torch.empty_like(values).to(rank) for _ in range(world_size)]
+    dist.all_gather(all_values, values)
+    cat_function = torch.cat if values.dim() > 0 else torch.stack
+    return cat_function(all_values, dim=0)
+
+
+def slice_and_move_batch_for_device(batch: Dict, rank: int, world_size: int, device: str) -> Dict:
+    """Slice a batch into chunks, and move each chunk to the specified device."""
+    chunk_size = len(list(batch.values())[0]) // world_size
+    start = chunk_size * rank
+    end = chunk_size * (rank + 1)
+    sliced = {k: v[start:end] for k, v in batch.items()}
+    on_device = {k: (v.to(device) if isinstance(v, torch.Tensor) else v) for k, v in sliced.items()}
+    return on_device
+
+
+def formatted_dict(d: Dict) -> Dict:
+    """Format a dictionary for printing."""
+    return {k: (f"{v:.5g}" if type(v) == float else v) for k, v in d.items()}
+
+
+def get_block_class_from_model(model: torch.nn.Module, block_class_name: str) -> torch.nn.Module:
+    """Get the class of a block from a model, using the block's class name."""
+    for module in model.modules():
+        if module.__class__.__name__ == block_class_name:
+            return module.__class__
+    raise ValueError(f"Could not find block class {block_class_name} in model {model}")
+
+
+def get_local_run_dir(exp_name: str, local_dirs: List[str]) -> str:
+    """Create a local directory to store outputs for this run, and return its path."""
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S_%f")
+    run_dir = f"{get_local_dir(local_dirs)}/{exp_name}_{timestamp}"
+    os.makedirs(run_dir, exist_ok=True)
+    return run_dir
+
+
+def init_distributed(rank: int, world_size: int, master_addr: str = 'localhost', port: int = 12355, backend: str = 'nccl'):
+    print(rank, 'initializing distributed')
+    os.environ["MASTER_ADDR"] = master_addr
+    os.environ["MASTER_PORT"] = str(port)
+    dist.init_process_group(backend, rank=rank, world_size=world_size)
+    torch.cuda.set_device(rank)
+
+
+def get_open_port():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', 0)) # bind to all interfaces and use an OS provided port
+        return s.getsockname()[1] # return only the port number
+
+
+def disable_dropout(model: torch.nn.Module):
+    """Disable dropout in a model."""
+    for module in model.modules():
+        if isinstance(module, torch.nn.Dropout):
+            module.p = 0
+
+
+class TemporarilySeededRandom:
+    def __init__(self, seed):
+        """Temporarily set the random seed, and then restore it when exiting the context."""
+        self.seed = seed
+        self.stored_state = None
+        self.stored_np_state = None
+
+    def __enter__(self):
+        # Store the current random state
+        self.stored_state = random.getstate()
+        self.stored_np_state = np.random.get_state()
+
+        # Set the random seed
+        random.seed(self.seed)
+        np.random.seed(self.seed)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Restore the random state
+        random.setstate(self.stored_state)
+        np.random.set_state(self.stored_np_state)
 
 
 if __name__ == "__main__":
---
+++ ChatGLM3-6B-Finetuning/tools/preference_datasets.py
@@ -0,0 +1,274 @@
+import datasets
+import torch
+from tools.common_utils import TemporarilySeededRandom
+from torch.nn.utils.rnn import pad_sequence
+from collections import defaultdict
+import tqdm
+import random
+from bs4 import BeautifulSoup, NavigableString
+import numpy as np
+from typing import Dict, List, Optional, Iterator, Callable, Union, Tuple
+
+
+def get_dataset(data_dir: str, data_files: str, split: str, split_ratio: float = 0.05, silent: bool = False) -> Dict[str, Dict[str, Union[List[Tuple[int, int]], List[str], str]]]:
+    r"""
+        将csv数据转换成dpo训练需要的数据集，并且根据split进行分割。
+        test、eval的分割比例为0.05
+        
+        Args:
+            params (`data_dir`): csv文件存放的路径
+            params (`data_files`): csv文件名
+            params (`split`): 分割数据集，取值为train、test、eval
+            params (`split_ratio`): 分割比例
+            params (`silent`): 是否静默处理
+    """
+    dataset = datasets.load_dataset(path=data_dir, data_files=data_files, split="train")
+    
+    dataset = dataset.shuffle(seed=42)
+    dataset = dataset.select(range(int(len(dataset) * split_ratio))) if split == 'test' or split == 'eval' else dataset.select(range(int(len(dataset) * split_ratio), len(dataset)))
+    
+    data = defaultdict(lambda: defaultdict(list))
+    for row in tqdm.tqdm(dataset, desc='Processing ' + split + " data", disable=silent):
+        prompt = '\n\nHuman: ' + row['query'] + '\n\nAssistant:'
+        label = row["label"]
+        predict = row["predict"]
+        
+        responses = [label, predict]
+        
+        # 因为偏好数据集的排序是选中的在前面，而这个数据集本身只会有两个选项，所以固定写成(1, 2)。
+        data[prompt]['pairs'].append((1, 2))
+        data[prompt]['responses'].extend(responses)
+        
+    assert set(list(data.values())[0].keys()) == {'responses', 'pairs'}, f"Unexpected keys in dataset: {list(list(data.values())[0].keys())}"
+
+    return data
+
+
+def get_collate_fn(tokenizer) -> Callable[[List[Dict]], Dict[str, Union[List, torch.Tensor]]]:
+    """Returns a collate function for the given tokenizer.
+    
+       The collate function takes a list of examples (dicts, where values are lists of
+         ints [tokens] or strings [the original texts]) and returns a batch of examples,
+         PyTorch tensors padded to the maximum length. Strings are passed through."""
+    def collate_fn(batch):
+        # first, pad everything to the same length
+        padded_batch = {}
+        for k in batch[0].keys():
+            if k.endswith('_input_ids') or k.endswith('_attention_mask') or k.endswith('_labels'):
+                if 'prompt' in k:  # adapted from https://stackoverflow.com/questions/73256206
+                    to_pad = [torch.LongTensor(ex[k][::-1]) for ex in batch]
+                else:
+                    to_pad = [torch.LongTensor(ex[k]) for ex in batch]
+                if k.endswith('_input_ids'):
+                    padding_value = tokenizer.pad_token_id
+                elif k.endswith('_labels'):
+                    padding_value = -100
+                elif k.endswith('_attention_mask'):
+                    padding_value = 0
+                else:
+                    raise ValueError(f"Unexpected key in batch '{k}'")
+
+                padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
+                if 'prompt' in k:  # for the prompt, flip back so padding is on left side
+                    padded_batch[k] = padded_batch[k].flip(dims=[1])
+            else:
+                padded_batch[k] = [ex[k] for ex in batch]
+
+        return padded_batch
+    return collate_fn
+
+
+def tokenize_batch_element(prompt: str, chosen: str, rejected: str, truncation_mode: str, tokenizer, max_length: int, max_prompt_length: int) -> Dict:
+    """Tokenize a single batch element.
+    
+       At this stage, we don't convert to PyTorch tensors yet; we just handle the truncation
+         in case the prompt + chosen or prompt + rejected responses is/are too long. First
+         we truncate the prompt; if we're still too long, we truncate the chosen/rejected.
+       
+       We also create the labels for the chosen/rejected responses, which are of length equal to
+         the sum of the length of the prompt and the chosen/rejected response, with -100 for the
+         prompt tokens.
+    """
+    chosen_tokens = tokenizer(chosen, add_special_tokens=False)
+    rejected_tokens = tokenizer(rejected, add_special_tokens=False)
+    prompt_tokens = tokenizer(prompt, add_special_tokens=False)
+
+    assert tokenizer.eos_token_id not in prompt_tokens['input_ids'], f"Prompt contains EOS token: {prompt}"
+    assert tokenizer.eos_token_id not in chosen_tokens['input_ids'], f"Chosen response contains EOS token: {chosen}"
+    assert tokenizer.eos_token_id not in rejected_tokens['input_ids'], f"Rejected response contains EOS token: {rejected}"
+
+    chosen_tokens['input_ids'].append(tokenizer.eos_token_id)
+    chosen_tokens['attention_mask'].append(1)
+
+    rejected_tokens['input_ids'].append(tokenizer.eos_token_id)
+    rejected_tokens['attention_mask'].append(1)
+
+    longer_response_length = max(len(chosen_tokens['input_ids']), len(rejected_tokens['input_ids']))
+
+    # if combined sequence is too long, truncate the prompt
+    if len(prompt_tokens['input_ids']) + longer_response_length > max_length:
+        if truncation_mode == 'keep_start':
+            prompt_tokens = {k: v[:max_prompt_length] for k, v in prompt_tokens.items()}
+        elif truncation_mode == 'keep_end':
+            prompt_tokens = {k: v[-max_prompt_length:] for k, v in prompt_tokens.items()}
+        else:
+            raise ValueError(f'Unknown truncation mode: {truncation_mode}')
+
+    # if that's still too long, truncate the response
+    if len(prompt_tokens['input_ids']) + longer_response_length > max_length:
+        chosen_tokens = {k: v[:max_length - max_prompt_length] for k, v in chosen_tokens.items()}
+        rejected_tokens = {k: v[:max_length - max_prompt_length] for k, v in rejected_tokens.items()}
+
+    # Create labels
+    chosen_sequence_tokens = {k: prompt_tokens[k] + chosen_tokens[k] for k in chosen_tokens}
+    rejected_sequence_tokens = {k: prompt_tokens[k] + rejected_tokens[k] for k in rejected_tokens}
+    chosen_sequence_tokens['labels'] = chosen_sequence_tokens['input_ids'][:]
+    chosen_sequence_tokens['labels'][:len(prompt_tokens['input_ids'])] = [-100] * len(prompt_tokens['input_ids'])
+    rejected_sequence_tokens['labels'] = rejected_sequence_tokens['input_ids'][:]
+    rejected_sequence_tokens['labels'][:len(prompt_tokens['input_ids'])] = [-100] * len(prompt_tokens['input_ids'])
+
+    batch = {}
+
+    batch['prompt'] = prompt
+    batch['chosen'] = prompt + chosen
+    batch['rejected'] = prompt + rejected
+    batch['chosen_response_only'] = chosen
+    batch['rejected_response_only'] = rejected
+
+    for k, toks in {'chosen': chosen_sequence_tokens, 'rejected': rejected_sequence_tokens, 'prompt': prompt_tokens}.items():
+        for type_key, tokens in toks.items():
+            if type_key == 'token_type_ids':
+                continue
+            batch[f'{k}_{type_key}'] = tokens
+
+    return batch
+
+
+def get_batch_iterator(data_dir: str, 
+                       data_files: str,
+                       tokenizer,
+                       split: str = 'train',
+                       batch_size: int = 1,
+                       shuffle: bool = True,
+                       max_length: int = 1024,
+                       max_prompt_length: int = 1024,
+                       n_epochs: Optional[int] = None,
+                       n_examples: Optional[int] = None,
+                       seed:int = 0,
+                       silent: bool = False) -> Iterator[Dict]:
+    """Get an iterator over batches of data. Stops after n_epochs or n_examples, whichever comes first.
+
+    Args:
+        names: Names of datasets to use.
+        tokenizer: Tokenizer to use.
+        split: Which split to use.
+        batch_size: Batch size.
+        shuffle: Whether to shuffle the data after each epoch.
+        max_length: Maximum length of the combined prompt + response.
+        max_prompt_length: Maximum length of the prompt.
+        n_epochs: Number of epochs to run for. This or n_examples must be specified.
+        n_examples: Number of examples to run for. This or n_epochs must be specified.
+        seed: Random seed.
+        silent: Whether to silence the progress bar(s).
+        cache_dir: Directory to cache the datasets in.
+    """
+    assert n_epochs is not None or n_examples is not None, "Must specify either n_epochs or n_examples"
+    if silent:
+        datasets.logging.disable_progress_bar()
+        datasets.logging.set_verbosity_error()
+
+    with TemporarilySeededRandom(seed):
+        permutation_seeds = iter(np.random.randint(0, 2**32, size=1000000))
+        flat_data = []
+        truncation_mode = 'keep_start'
+        for prompt, data in get_dataset(data_dir = data_dir, data_files=data_files, split = split, silent = silent).items():
+            flat_data.append((prompt, data['responses'], data['pairs'], truncation_mode))
+            
+    collate_fn = get_collate_fn(tokenizer)
+
+    epoch_idx = 0
+    example_idx = 0
+    done = False
+    while True:
+        if n_epochs is not None and epoch_idx >= n_epochs:
+            if not silent:
+                print(f'Finished generating {n_epochs} epochs on {split} split')
+            break
+        if shuffle:
+            with TemporarilySeededRandom(next(permutation_seeds)):
+                random.shuffle(flat_data)
+
+        batch = []
+        for prompt, responses, pairs, truncation_mode in flat_data:
+            if done:
+                break
+            for p in pairs:
+                if done:
+                    break
+                batch_element = tokenize_batch_element(prompt, responses[p[0]], responses[p[1]], truncation_mode, tokenizer, max_length, max_prompt_length)
+                batch.append(batch_element)
+                example_idx += 1
+                if len(batch) == batch_size:
+                    yield collate_fn(batch)
+                    if n_examples is not None and example_idx >= n_examples:
+                        if not silent:
+                            print(f'FINISHED {n_examples} EXAMPLES on {split} split')
+                        done = True
+                    batch = []
+                
+        if done:
+            break
+
+        epoch_idx += 1
+        
+        
+def strings_match_up_to_spaces(str_a: str, str_b: str) -> bool:
+    """Returns True if str_a and str_b match up to spaces, False otherwise."""
+    for idx in range(min(len(str_a), len(str_b)) - 2):
+        if str_a[idx] != str_b[idx]:
+            if str_a[idx] != ' ' and str_b[idx] != ' ':
+                return False
+            else:
+                if str_a[idx] == ' ':
+                    str_a = str_a[:idx] + str_a[idx + 1:]
+                else:
+                    str_b = str_b[:idx] + str_b[idx + 1:]
+
+    return True
+
+
+def strip_html_tags(html_string):
+    """Strip HTML tags from a string, except for <code> tags (which contain real code in the StackExchange answers)."""
+    # Create a BeautifulSoup object
+    soup = BeautifulSoup(html_string, 'html.parser')
+
+    # Initialize an empty list to store the text
+    text = []
+    for element in soup.children:
+        if isinstance(element, NavigableString):
+            continue
+        if element.name == 'p':
+            text.append(''.join(child.string for child in element.children if isinstance(child, NavigableString)))
+        elif element.name == 'pre':
+            for code in element.find_all('code'):
+                text.append("<code>" + code.get_text() + "</code>")
+        elif element.name == 'code':
+            text.append("<code>" + element.get_text() + "</code>")
+
+    # Join the text together with newlines in between
+    text = "\n\n".join(text)
+
+    return text
+
+
+def extract_anthropic_prompt(prompt_and_response):
+    """Extract the anthropic prompt from a prompt and response pair."""
+    search_term = '\n\nAssistant:'
+    search_term_idx = prompt_and_response.rfind(search_term)
+    assert search_term_idx != -1, f"Prompt and response does not contain '{search_term}'"
+    return prompt_and_response[:search_term_idx + len(search_term)]
+
+
+if __name__ == "__main__":
+    data = get_dataset(data_dir="../data", data_files="train_err.csv", split="test")
+    print(data)
